9fceb58e679e55aa8778d820ed6675415268813c
Mateus Rodrigues <rodriguesjmateus@gmail.com>
2025-04-06 22:37:19 -0300

    LastCommit

--- database/migrations/2025_04_05_001101_create_postagens_table.php
+++ database/migrations/2025_04_05_001101_create_postagens_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categorias', function (Blueprint $table) {
+        Schema::create('postagens', function (Blueprint $table) {
             $table->id();
-            $table-> string('nome');
+            $table->bigInteger('categoria_id');
+            $table->string('titulo');
+            $table->longtext('descricao');
             $table->timestamps();
         });
     }
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categorias');
+        Schema::dropIfExists('postagens');
     }
 };
